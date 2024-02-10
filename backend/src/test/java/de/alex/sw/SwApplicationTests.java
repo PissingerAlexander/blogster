70f55616fb5dc6a83f18c15ffa2b003edc9888fb
Alexander Pissinger <alex.pissinger@gmail.com>
2024-02-10 19:53:17 +0100

    spring initializer

---
+++ backend/src/test/java/de/alex/sw/SwApplicationTests.java
@@ -0,0 +1,13 @@
+package de.alex.sw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
