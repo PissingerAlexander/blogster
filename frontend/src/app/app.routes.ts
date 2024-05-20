89906ec82b48003073a7b3d525555cac5667fa24
alex <alex.pissinger@gmail.com>
2024-05-20 08:42:12 +0200

    removed unused register component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,11 +1,9 @@
 import { Routes } from '@angular/router';
 import { IndexComponent } from "./pages/index/index.component";
 import { LoginComponent } from "./components/login/login.component";
-import { RegisterComponent } from "./components/register/register.component";
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent }, // TODO: LoginPageComponent
-  { path: 'register', component: RegisterComponent }, // TODO: RegisterPageComponent
   { path: 'index', component: IndexComponent },
   { path: '**', component: IndexComponent }
 ];
