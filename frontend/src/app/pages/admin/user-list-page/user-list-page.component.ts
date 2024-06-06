f553c803ac7a255ab65f2a0e4e53658cc423326e
alex <alex.pissinger@gmail.com>
2024-06-06 08:18:19 +0200

    created user list page for admin

--- frontend/src/app/pages/admin/user-list-page/user-list-page.component.ts
+++ frontend/src/app/pages/admin/user-list-page/user-list-page.component.ts
@@ -1,15 +1,69 @@
-import {Component} from '@angular/core';
+import {AfterViewInit, Component, ViewChild} from '@angular/core';
 import {HeaderComponent} from "../../../components/header/header.component";
+import {UserService} from "../../../services/api/user.service";
+import {MatButton, MatIconButton, MatMiniFabButton} from "@angular/material/button";
+import {MatList, MatListItem} from "@angular/material/list";
+import {User} from "../../../model/user/user";
+import {
+  MatCell,
+  MatCellDef,
+  MatColumnDef,
+  MatHeaderCell,
+  MatHeaderCellDef,
+  MatHeaderRow, MatHeaderRowDef, MatRow, MatRowDef,
+  MatTable, MatTableDataSource
+} from "@angular/material/table";
+import {MatIcon} from "@angular/material/icon";
+import {catchError} from "rxjs";
+import {Router} from "@angular/router";
+import {MatPaginator} from "@angular/material/paginator";
 
 @Component({
   selector: 'app-user-list-page',
   standalone: true,
   imports: [
-    HeaderComponent
+    HeaderComponent,
+    MatButton,
+    MatIcon,
+    MatList,
+    MatListItem,
+    MatTable,
+    MatColumnDef,
+    MatHeaderCell,
+    MatCell,
+    MatHeaderCellDef,
+    MatCellDef,
+    MatHeaderRow,
+    MatRow,
+    MatHeaderRowDef,
+    MatRowDef,
+    MatMiniFabButton,
+    MatIconButton,
+    MatPaginator
   ],
   templateUrl: './user-list-page.component.html',
   styleUrl: './user-list-page.component.scss'
 })
 export class UserListPageComponent {
+  userList: User[] = [];
+  displayedColumns: string[] = ['id', 'username', 'fullName', 'mailAddress', 'role', 'remove'];
 
+
+  constructor(private userService: UserService) {
+    userService.getAllUsers().subscribe((data: User[]) => {
+      this.userList = data;
+    })
+  }
+
+  getCurrentUsername(): string {
+    return this.userService.getCurrentUser().username;
+  }
+
+  deleteUser(user: User): void {
+    this.userService.deleteUser(user)
+      .pipe(catchError(this.userService.handleError))
+      .subscribe((): void => {
+        this.userList.splice(this.userList.indexOf(user), 1);
+      });
+  }
 }
