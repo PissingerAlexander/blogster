1ff6dd43294425d505540fe7b1ed03b9622d4a21
alex <alex.pissinger@gmail.com>
2024-06-09 09:54:07 +0200

    added creating blog button and dialog

--- frontend/src/app/components/blogs/blog-list/blog-list.component.ts
+++ frontend/src/app/components/blogs/blog-list/blog-list.component.ts
@@ -1,7 +1,7 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {BlogService} from "../../../services/api/blog.service";
-import {catchError, throwError} from "rxjs";
+import {catchError, Subject, throwError} from "rxjs";
 import {HttpErrorResponse} from "@angular/common/http";
 import {Blog} from "../../../model/blog/blog";
 
@@ -13,22 +13,29 @@ import {Blog} from "../../../model/blog/blog";
   styleUrl: './blog-list.component.scss'
 })
 export class BlogListComponent implements OnInit {
+  userId: number | undefined;
   blogList: Blog[] = [];
 
   constructor(private route: ActivatedRoute, private blogService: BlogService) {
   }
 
+  updateBlogList() {
+    if (!this.userId) return;
+    this.blogService.getAllBlogs(this.userId)
+      .pipe(catchError((error: HttpErrorResponse) => {
+        console.error(error.error);
+        // TODO: display info about error to user directly (on the form?)
+        return throwError(() => new Error('Something bad happened; please try again'));
+      }))
+      .subscribe((response: Blog[]) => {
+        this.blogList = response;
+      })
+  }
+
   ngOnInit() {
     this.route.params.subscribe((parameterList) => {
-      this.blogService.getAllBlogs(parameterList['userId'])
-        .pipe(catchError((error: HttpErrorResponse) => {
-          console.error(error.error);
-
-          return throwError(() => new Error('Something bad happened; please try again'));
-        }))
-        .subscribe((response: Blog[]) => {
-          this.blogList = response;
-      })
+      this.userId = parameterList['userId'];
     })
+    this.updateBlogList();
   }
 }
--- frontend/src/app/components/blogs/create-blog/create-blog.component.ts
+++ frontend/src/app/components/blogs/create-blog/create-blog.component.ts
@@ -1,11 +1,25 @@
-import {Component} from '@angular/core';
-import {MatButton} from "@angular/material/button";
+import {Component, OnInit} from '@angular/core';
+import {MatButton, MatIconButton} from "@angular/material/button";
 import {BlogService} from "../../../services/api/blog.service";
 import {CreateBlogRequest} from "../../../model/blog/http/create_blog/CreateBlogRequest";
-import {FormsModule} from "@angular/forms";
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 import {catchError, throwError} from "rxjs";
 import {HttpErrorResponse} from "@angular/common/http";
-import {CreateBlogResponse} from "../../../model/blog/http/create_blog/CreateBlogResponse";
+import {
+  MatDialog,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogRef,
+  MatDialogTitle
+} from "@angular/material/dialog";
+import {MatError, MatFormField, MatLabel, MatSuffix} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {ActivatedRoute, Router} from "@angular/router";
+import {MatIcon} from "@angular/material/icon";
+import {MatOption, MatSelect} from "@angular/material/select";
+import {UserService} from "../../../services/api/user.service";
+import {AuthService} from "../../../services/auth/auth.service";
 
 @Component({
   selector: 'app-create-blog',
@@ -17,26 +31,86 @@ import {CreateBlogResponse} from "../../../model/blog/http/create_blog/CreateBlo
   templateUrl: './create-blog.component.html',
   styleUrl: './create-blog.component.scss'
 })
-export class CreateBlogComponent {
-  public userId: number | undefined;
-  public blogName: string = "";
+export class CreateBlogComponent implements OnInit {
+  userId: number | undefined;
+  currentUserId: number | undefined;
 
-  constructor(private blogService: BlogService) {
+  constructor(private router: Router, private activatedRoute: ActivatedRoute, private authService: AuthService, public dialog: MatDialog) {
   }
 
-  public createBlog() {
-    console.log(this.blogName);
-    if (this.blogName == "") return;
+  openDialog() {
+    const dialogRef = this.dialog.open(CreateBlogDialog)
+    dialogRef.afterClosed().subscribe((result: number) => {
+      console.log(this.router.url);
+      if (result) this.router.navigate([this.router.url, result]).then();
+    })
+  }
+
+  ngOnInit() {
+    this.activatedRoute.params.subscribe(parameterList => {
+      this.userId = parameterList['userId'];
+    });
+    this.currentUserId = this.authService.getId();
+  }
+}
+
+@Component({
+  selector: 'create-blog-dialog',
+  standalone: true,
+  imports: [
+    MatDialogTitle,
+    MatDialogContent,
+    MatButton,
+    MatDialogActions,
+    MatDialogClose,
+    FormsModule,
+    MatError,
+    MatFormField,
+    MatIcon,
+    MatIconButton,
+    MatInput,
+    MatLabel,
+    MatSuffix,
+    ReactiveFormsModule,
+    MatSelect,
+    MatOption
+  ],
+  templateUrl: './create-blog-dialog/create-blog.dialog.html',
+  styleUrl: './create-blog-dialog/create-blog.dialog.scss'
+})
+export class CreateBlogDialog {
+  createBlogForm = new FormGroup({
+    blogName: new FormControl<string>('', [Validators.required])
+  })
+  blogNameErrorMessage = '';
+
+  constructor(
+    private blogService: BlogService,
+    public dialogRef: MatDialogRef<CreateBlogDialog>
+  ) {
+  }
+
+  updateBlogNameErrorMessage() {
+    if (this.createBlogForm.controls.blogName.hasError('required')) {
+      this.blogNameErrorMessage = 'You need to enter your blog name';
+    } else {
+      this.blogNameErrorMessage = '';
+    }
+  }
+
+  createBlog() {
     let createBlogRequest: CreateBlogRequest = {
-      blogName: this.blogName
+      blogName: this.createBlogForm.controls.blogName.value!
     }
     this.blogService.createBlog(createBlogRequest)
       .pipe(catchError((error: HttpErrorResponse) => {
-        console.error(error);
+        // TODO: display info about error to user directly (on the form?)
+        console.error(error.error);
         return throwError(() => new Error('Something bad happened; please try again'));
       }))
-      .subscribe((blog: CreateBlogResponse) => {
-        console.log(blog);
-      })
+      .subscribe((res) => {
+        this.dialogRef.close(res.data!.id);
+        this.createBlogForm.reset();
+      });
   }
 }
