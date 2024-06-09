025756e803b6402531f0f3db2131303b05299ad9
alex <alex.pissinger@gmail.com>
2024-06-09 09:51:01 +0200

    insert 5 user by default

--- backend/src/main/java/de/alex/blogster_rest_api/security/config/DefaultAdminConfiguration.java
+++ backend/src/main/java/de/alex/blogster_rest_api/security/config/DefaultAdminConfiguration.java
@@ -7,6 +7,8 @@ import de.alex.blogster_rest_api.user.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.ArrayList;
+
 @Configuration
 public class DefaultAdminConfiguration {
     private final UserService userService;
@@ -28,6 +30,19 @@ public class DefaultAdminConfiguration {
             admin.setRole(Role.ROLE_ADMIN);
             userService.createUser(admin);
         }
+
+        // add a few users | TODO: remove
+        ArrayList<User> users = userService.findAllUsers();
+        while (users.size() <= 5) {
+            userService.createUser(new User(
+                    "user" + users.size(),
+                    "password",
+                    null,
+                    "user" + users.size() + "@blogsterpage.com"
+            ));
+            users = userService.findAllUsers();
+        }
+
         return userService.findUserByUsername("admin");
     }
 }
