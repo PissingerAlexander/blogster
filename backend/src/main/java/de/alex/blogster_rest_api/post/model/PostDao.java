5c85cce5a79c5598f4b6563415d85d6d7438b9b1
alex <alex.pissinger@gmail.com>
2024-06-13 14:19:51 +0200

    post/core: added missing functions

--- backend/src/main/java/de/alex/blogster_rest_api/post/model/PostDao.java
+++ backend/src/main/java/de/alex/blogster_rest_api/post/model/PostDao.java
@@ -1,32 +1,58 @@
 package de.alex.blogster_rest_api.post.model;
 
 import de.alex.blogster_rest_api.Dao;
+import de.alex.blogster_rest_api.post.repository.PostRepository;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 
+@Component
 public class PostDao implements Dao<Post> {
+    private final PostRepository postRepository;
+
+    public PostDao(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
     @Override
     public Post findById(long id) {
-        return null;
+        return postRepository.findById(id);
     }
 
     @Override
     public ArrayList<Post> findAll() {
-        return null;
+        return (ArrayList<Post>) postRepository.findAll();
     }
 
     @Override
     public Post getPage(int size, int page) {
+        //TODO: implement!
         return null;
     }
 
     @Override
     public Post save(Post post) {
-        return null;
+        return postRepository.save(post);
     }
 
     @Override
     public Post deleteById(long id) {
-        return null;
+        return postRepository.deleteById(id);
+    }
+
+    public Post findByTitle(String title) {
+        return postRepository.findByPostTitleIgnoreCase(title);
+    }
+
+    public ArrayList<Post> findByBlogId(long id) {
+        return postRepository.findByBlog_Id(id);
+    }
+
+    public Post findByPostTitleAndBlogId(String postTitle, long blogId) {
+        return postRepository.findByPostTitleIgnoreCaseAndBlog_Id(postTitle, blogId);
+    }
+
+    public void deletePostsByBlogId(long blogId) {
+        postRepository.deleteByBlog_Id(blogId);
     }
 }
